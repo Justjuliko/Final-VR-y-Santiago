4ed37ea5fe68471f8c5692da81c6a4f3f0699704
Julio Echavarría <83980281+Justjuliko@users.noreply.github.com>
2023-10-31 18:08:59 -0500

    Puzzle2 Completo

--- Assets/AAAProyectoFinal/Scripts/Puzzle2Controller.cs
+++ Assets/AAAProyectoFinal/Scripts/Puzzle2Controller.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Puzzle2Controller : MonoBehaviour
 {
     public GameObject code;
     [SerializeField] bool codeVisible;
+
+    string Code = "623";
+    string Nr = null;
+    int NrIndex = 0;
+    string alpha;
+    public TMP_Text UIText = null;
+
+    public GameObject unlockable3;
+    public GameObject unlockable4;
 
     public void ShowCode()
     {
@@ -16,5 +26,31 @@ public class Puzzle2Controller : MonoBehaviour
     {
         code.SetActive(false);
         codeVisible = false;
+    }
+    public void CodeFunction(string Numbers)
+    {
+        NrIndex++;
+        Nr = Nr + Numbers;
+        UIText.text = Nr;
+    }
+    public void EnterCode()
+    {
+        if (Nr == Code)
+        {
+            unlockable3.SetActive(false);
+            unlockable4.SetActive(false);
+        }
+        else 
+        {
+            NrIndex++;
+            Nr = null;
+            UIText.text = Nr;
+        }
+    }
+    public void DeleteCode()
+    {
+        NrIndex++;
+        Nr = null;
+        UIText.text = Nr;
     }
 }
